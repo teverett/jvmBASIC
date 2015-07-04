4eb7cd3756a40293f16e51cf56b4a5bda127c5ae
Tom Everett <tom@khubla.com>
2015-07-04 11:29:13 -0600

    updated commons cli

--- jvmbasicwww/src/main/java/com/khubla/jvmbasic/jvmbasicwww/JVMBASICWWW.java
+++ jvmbasicwww/src/main/java/com/khubla/jvmbasic/jvmbasicwww/JVMBASICWWW.java
@@ -18,11 +18,10 @@ package com.khubla.jvmbasic.jvmbasicwww;
  */
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.PosixParser;
 
 /**
  * @author tome
@@ -48,28 +47,16 @@ public class JVMBASICWWW {
           * options
           */
          final Options options = new Options();
-         OptionBuilder.withArgName(SOURCEDIR_OPTION);
-         OptionBuilder.isRequired();
-         OptionBuilder.hasArg();
-         OptionBuilder.withDescription("source dir");
-         final Option sourcediroption = OptionBuilder.create(SOURCEDIR_OPTION);
-         options.addOption(sourcediroption);
-         OptionBuilder.withArgName(CLASSDIR_OPTION);
-         OptionBuilder.isRequired();
-         OptionBuilder.hasArg();
-         OptionBuilder.withDescription("class dir");
-         final Option clso = OptionBuilder.create(CLASSDIR_OPTION);
-         options.addOption(clso);
-         OptionBuilder.isRequired(false);
-         OptionBuilder.withArgName(PORT_OPTION);
-         OptionBuilder.hasArg();
-         OptionBuilder.withDescription("TCP port");
-         final Option po = OptionBuilder.create(PORT_OPTION);
-         options.addOption(po);
+         Option oo = Option.builder().argName(SOURCEDIR_OPTION).longOpt(SOURCEDIR_OPTION).type(String.class).hasArg().required(false).desc("source dir").build();
+         options.addOption(oo);
+         oo = Option.builder().argName(CLASSDIR_OPTION).longOpt(CLASSDIR_OPTION).type(String.class).hasArg().required(false).desc("class dir").build();
+         options.addOption(oo);
+         oo = Option.builder().argName(PORT_OPTION).longOpt(PORT_OPTION).type(String.class).hasArg().required(false).desc("TCP port").build();
+         options.addOption(oo);
          /*
           * parse
           */
-         final CommandLineParser parser = new PosixParser();
+         final CommandLineParser parser = new DefaultParser();
          CommandLine cmd = null;
          try {
             cmd = parser.parse(options, args);
