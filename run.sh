970d3b9dd6079231dcc5df35a85b068d4a8d2ad6
Tom Everett <tom@khubla.com>
2019-01-07 18:58:38 -0700

    fixed run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ cp jvmbasicc/target/jvmbasicc.jar eggsample/
 
 cd eggsample
 java -jar jvmbasicc.jar -file ../jvmbasicc/src/main/resources/examples/print.bas
-jar cvfme print.jar ../jvmbasicc/src/main/resources/manifest.txt PRINT PRINT.class 
+jar cvfme print.jar ../jvmbasicc/src/main/resources/manifest.txt print print.class 
 
 jar tf print.jar
 javap -p print
